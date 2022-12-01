2684a6952896d104eb2dc93f3df9aa3949a5de35
besyildiz <mehterli@gmail.com>
2022-12-01 14:23:04 +0300

    classslar

---
+++ uygulama/class5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama
+{
+    public partial class class5 : Form
+    {
+        public class5()
+        {
+            InitializeComponent();
+        }
+        class SayiIslemleri
+        {
+            public int Topla(int sayi1, int sayi2, int sayi3)
+            {
+                return sayi1 + sayi2 + sayi3;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SayiIslemleri si = new SayiIslemleri();
+            // int toplam = si.Topla(5, 10, 15);
+            int toplam = si.Topla(sayi2: 10, sayi3: 15, sayi1: 5);
+            listBox1.Items.Add("Toplam: "+ toplam);
+        }
+    }
+}
---
+++ uygulama/class6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama
+{
+    public partial class class6 : Form
+    {
+        public class6()
+        {
+            InitializeComponent();
+        }
+        class SayiIslemleri
+        {
+            public int Toplam(params int[] sayilar)
+            {
+                int toplam = 0;
+                foreach (var s in sayilar)
+                {
+                    toplam += s;
+                }
+                return toplam;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SayiIslemleri si = new SayiIslemleri();
+            listBox1.Items.Add("Toplam: "+ si.Toplam(3));
+            listBox1.Items.Add("Toplam: "+ si.Toplam(3, 4, 5));
+            listBox1.Items.Add("Toplam: "+ si.Toplam(5, 1, 7, 3, 4, 5));
+        }
+    }
+}
